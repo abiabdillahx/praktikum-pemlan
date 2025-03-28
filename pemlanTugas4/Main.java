3f8d851cb6b7705da8bc97fd0205237e7b00aa85
Abi <83850574+abiabdillahx@users.noreply.github.com>
2025-03-28 17:28:48 +0700

    refactor: added all transaction system and main class

--- pemlanTugas4/Main.java
+++ pemlanTugas4/Main.java
@@ -1,27 +1,75 @@
 import java.util.Scanner;
 
 public class Main {
+    private static Akun[] dataAkun = {
+        new Akun("3800001111", "Dung dung", 8000, 111000), // --> sebagai contoh kasus jika saldo dibawah 10rb
+        new Akun("3800001112", "Ahmad", 400000, 101010), // --> sebagai contoh akun silver
+        new Akun("5600002222", "Mulyono", 700000, 696969), // --> sebagai contoh akun gold
+        new Akun("7400003333", "Niggus", 2000000, 989898) // --> sebagai contoh akun platinum
+    };
+
     public static void main(String[] args) {
-        System.out.println("Selamat Datang di Swalayan X!");
-        System.out.println("==============================");
-        System.out.println("1. Top up\n2. Belanja\n3. Keluar");
-        System.out.println("==============================");
-        System.out.print("Pilih Menu");
-        Scanner sc = new Scanner(System.in);
-        int pilihan = sc.nextInt();
+        
+        int pilihan;
+        System.out.println("> Selamat Datang di Swalayan X!");
+        do { 
+            System.out.println("==============================");
+            System.out.println("1. Top up\n2. Belanja\n3. Keluar");
+            System.out.println("==============================");
+            System.out.print("Pilih Menu : ");
+            
+            Scanner sc = new Scanner(System.in);
+            if (!sc.hasNextInt()) {  // Cek apakah input bukan angka
+                System.out.println("> Input tidak valid! Masukkan angka 1-3.");
+                sc.next(); // Bersihin input
+                continue;
+            }
+        
+            pilihan = sc.nextInt();
+            sc.nextLine(); // Flush newline character
+        
+            if (pilihan == 3){
+                System.out.println("> Terima Kasih telah datang!\n");
+                break;
+            } else if (pilihan < 1 || pilihan > 3) { 
+                System.out.println("> Opsi tidak tersedia!"); 
+                continue; 
+            }
+            
+            // Jika semua kondisi di atas tidak terpenuhi, maka program akan melanjutkan ke bagian ini
+            System.out.print("Masukkan nomor pelanggan: ");
+            String noPel = sc.nextLine();   
+            Akun akunPelanggan = getAkun(noPel, dataAkun);
+        
+            if (akunPelanggan == null) { 
+                System.out.println("> Nomor pelanggan tidak ditemukan!");
+                continue;
+            } else if (akunPelanggan.getIsBlocked()) {
+                System.out.println("> Akun pelanggan telah diblokir! hubungi admin");
+                continue;
+            }
 
-        switch (pilihan) {
-            case 1:
-                topUp();
+            // Transaksi utamanya di sini        
+            switch (pilihan) {
+                case 1:
+                    Transaksi.topUp(akunPelanggan);
+                    break;
+                case 2:
+                    Transaksi.belanja(akunPelanggan);
+                    break;
+            }
+        } while (true);
+        
+    }
+
+    // method untuk cek akun pelanggan
+    public static Akun getAkun(String noPel, Akun[] dataAkun) {
+        for (Akun akun : dataAkun) {
+            if (akun.getNoPelanggan().equals(noPel)) {
+                return akun; // Ketemu
+            }
         }
+        return null; // Nggak ketemu
     }
-
-    public void topUp(){
-        Scanner in = new Scanner(System.in);
-        System.out.println("------------------");
-        System.out.print("Masukkan no pelanggan : ");
-        String noPelanggan = in.nextLine();
-        System.out.print("Masukkan PIN : ");
-        String pin = in.nextLine();
-    }
+        
 }
--- pemlanTugas4/Transaksi.java
+++ pemlanTugas4/Transaksi.java
@@ -1,14 +1,115 @@
+import java.util.Scanner;
+
 public class Transaksi {
-    public static void beliSilver(){
+    
+    public static void topUp(Akun akun){
+        Scanner in = new Scanner(System.in);
+        System.out.println("------------------");
+        int tries = 0;
+        int pin = 0;
+        String noPel = akun.getNoPelanggan();
+        while(tries < 3 || pin != akun.getPin()){
+            // System.out.println("percobaan = " + tries); // debug doang
+            System.out.print("Masukkan PIN : ");
+            pin = in.nextInt();
+            if(pin == akun.getPin()){
+                akun.infoAkun(noPel, pin);
+                System.out.print("> Masukkan jumlah uang yang ingin ditambahkan : ");
+                double uang = in.nextInt();
+                if(uang < 0){
+                    System.out.println("Maaf, jumlah uang tidak boleh minus");
+                    return;
+                }
+                akun.setSaldo(akun.getSaldo() + uang);
+                System.out.println("> Uang berhasil ditambahkan");
+                break;
 
+            }else{
+                System.out.println("> PIN salah");
+                tries++;
+            }
+            if(tries == 3){
+                System.out.println("> PIN salah sebanyak 3 kali, akun diblokir");
+                akun.setBlokir(true);
+                break;
+            }
+            
+        }
+        // System.out.println("------------------");
     }
-    public static void beliGold(){
 
-    }
-    public static void beliPlatinum(){
+    public static void belanja(Akun akun){
+        double saldoAkun = akun.getSaldo();
+        if (saldoAkun < 10000){
+            System.out.printf("Saldo anda Rp %,.2f %n", akun.getSaldo());
+            System.out.println("> Saldo tidak cukup, top up dulu yhh");
+            return;
+        }
+        Scanner ok = new Scanner(System.in);
+        System.out.println("------------------");
+        int tries = 0;
+        int pin = 0;
+        String noPel = akun.getNoPelanggan();
+        while(tries < 3 || pin != akun.getPin()){
+            System.out.print("Masukkan PIN : ");
+            pin = ok.nextInt();
+            if(pin == akun.getPin()){
+                akun.infoAkun(noPel, pin);
+                System.out.print("> Masukkan total harga belanja : ");
+                double belanja = ok.nextInt();
+                if(belanja < 0){
+                    System.out.println("Maaf, total harga belanja tidak boleh minus");
 
-    }
-    public void topUp(){
+                }else if (belanja > saldoAkun || (saldoAkun - belanja) < 10000){
+                    System.out.println("> Transaksi gagal, saldo anda tidak cukup!");
+                    return;
+                }
+                if(belanja > 0 && belanja <= saldoAkun){
+                    double cashback = calcCashback(akun.getLevel(noPel), belanja);
+                    akun.setSaldo(akun.getSaldo() - belanja + cashback);
+                    System.out.println("> Transaksi berhasil!");
+                    System.out.println("Total belanja : Rp " + (belanja - cashback));
+                    System.out.println("> Cashback : Rp " + cashback);
+                    System.out.println("> Saldo anda : Rp " + akun.getSaldo());
+                    break;
+                }
+
+            }else{
+                System.out.println("> PIN salah");
+                tries++;
+            }
+            if(tries == 3){
+                System.out.println("> PIN salah sebanyak 3 kali, akun diblokir");
+                akun.setBlokir(true);
+                break;
+            }
+            
+        }
         
+
+
     }
+
+    public static double calcCashback(String level, double belanja){
+        double cashback = 0;
+        if (level.equals("Silver") && belanja > 1000000){
+            cashback = belanja * 0.05;
+        }else if(level.equals("Gold")){
+            if(belanja > 1000000){
+                cashback = belanja * 0.07;
+            }else{
+                cashback = belanja * 0.02;
+            }
+        }else if(level.equals("Platinum")){
+            if (belanja > 1000000){
+                cashback = belanja * 0.10;
+            }else{
+                cashback = belanja * 0.05;
+            }
+        }else{
+            return 0;
+        }
+        return cashback;
+    }
+
 }
